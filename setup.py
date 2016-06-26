aa29ab4d2ee2fc0e7063a6f43301c86629881328
Amir Kadivar <amir@amirkdv.ca>
2016-06-26 19:20:20 -0400

    bump up to 0.2, fixup setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,24 @@
 
 from setuptools import setup, find_packages
 
+try:
+    from io import open
+except ImportError:
+    pass
+
 setup(
     name='sphinxcontrib-wiki',
-    version='0.1.1',
+    version='0.2',
     url='https://github.com/amirkdv/sphinxcontrib-wiki',
     license='MIT',
     author='Amir Kadivar',
     author_email='amir@amirkdv.ca',
     description='A simple wiki extension for Sphinx',
-    long_description='Allow wiki pages to be defined section by section in' +
-        ' docstrings and assembled on demand in a sphinx documents.',
+    long_description=open('README.rst', encoding='utf-8').read(),
     zip_safe=False,
+    # cf. https://pypi.python.org/pypi?:action=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        #   TODO cf. https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        #
-        #  'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta'
         'Environment :: Console',
         'Environment :: Web Environment',
         'Framework :: Sphinx :: Extension',
